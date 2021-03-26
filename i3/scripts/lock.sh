ff9d4668a28eac1350efd0a4222a963714d54181
LooseLunch <81433810+LooseLunch@users.noreply.github.com>
2021-03-26 16:54:42 +0000

    Add files via upload

---
+++ i3/scripts/lock.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+icon="$HOME/.config/i3/icon.png"
+tmpbg='/tmp/screen.png'
+xRes=$(echo 1280 - 623)
+
+(( $# )) && { icon=$1; }
+
+rm -f "$tmpbg"
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -geometry +1025+474 -composite -matte "$tmpbg"
+i3lock -u -i "$tmpbg"
